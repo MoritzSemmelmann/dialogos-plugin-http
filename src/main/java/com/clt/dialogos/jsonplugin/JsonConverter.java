b06537f79e29772fc710444b7252a5badb8f075a
MoritzSemmelmann <moritz.semmelmann@st.oth-regensburg.de>
2026-01-12 09:06:35 +0100

    Using dialogos json parser

--- src/main/java/com/clt/dialogos/jsonplugin/JsonConverter.java
+++ src/main/java/com/clt/dialogos/jsonplugin/JsonConverter.java
@@ -14,32 +14,28 @@ import java.util.function.Function;
 public class JsonConverter {
 
     public static Object valueToJson(Value value) {
-        if (value instanceof com.clt.script.exp.values.Undefined) {
+        String jsonString = value.toJson();
+        
+        if (jsonString.startsWith("{")) {
+            return new JSONObject(jsonString);
+        } else if (jsonString.startsWith("[")) {
+            return new JSONArray(jsonString);
+        } else if (jsonString.equals("null")) {
             return JSONObject.NULL;
-        } else if (value instanceof IntValue) {
-            return ((IntValue) value).getInt();
-        } else if (value instanceof RealValue) {
-            return ((RealValue) value).getReal();
-        } else if (value instanceof BoolValue) {
-            return ((BoolValue) value).getBool();
-        } else if (value instanceof StringValue) {
-            return ((StringValue) value).getString();
-        } else if (value instanceof ListValue) {
-            ListValue list = (ListValue) value;
-            JSONArray arr = new JSONArray();
-            for (int i = 0; i < list.size(); i++) {
-                arr.put(valueToJson(list.get(i)));
-            }
-            return arr;
-        } else if (value instanceof StructValue) {
-            StructValue struct = (StructValue) value;
-            JSONObject obj = new JSONObject();
-            for (String key : struct.getLabels()) {
-                obj.put(key, valueToJson(struct.getValue(key)));
-            }
-            return obj;
+        } else if (jsonString.equals("true") || jsonString.equals("false")) {
+            return Boolean.parseBoolean(jsonString);
+        } else if (jsonString.startsWith("\"")) {
+            return jsonString.substring(1, jsonString.length() - 1);
         } else {
-            return value.toString();
+            try {
+                if (jsonString.contains(".")) {
+                    return Double.parseDouble(jsonString);
+                } else {
+                    return Integer.parseInt(jsonString);
+                }
+            } catch (NumberFormatException e) {
+                return jsonString;
+            }
         }
     }
 
@@ -47,23 +43,11 @@ public class JsonConverter {
         if (json == null || json == JSONObject.NULL) {
             return new Undefined();
         } else if (json instanceof JSONObject) {
-            JSONObject obj = (JSONObject) json;
-            String[] labels = new String[obj.length()];
-            Value[] values = new Value[obj.length()];
-            int i = 0;
-            for (String key : obj.keySet()) {
-                labels[i] = key;
-                values[i] = jsonToValue(obj.get(key));
-                i++;
-            }
-            return new StructValue(labels, values);
+            return Value.fromJson((JSONObject) json);
         } else if (json instanceof JSONArray) {
-            JSONArray arr = (JSONArray) json;
-            Value[] values = new Value[arr.length()];
-            for (int i = 0; i < arr.length(); i++) {
-                values[i] = jsonToValue(arr.get(i));
-            }
-            return new ListValue(values);
+            return Value.fromJson((JSONArray) json);
+        } else if (json instanceof String) {
+            return new StringValue((String) json);
         } else if (json instanceof Boolean) {
             return new BoolValue((Boolean) json);
         } else if (json instanceof Integer) {
@@ -72,8 +56,6 @@ public class JsonConverter {
             return new IntValue(((Long) json).intValue());
         } else if (json instanceof Double) {
             return new RealValue((Double) json);
-        } else if (json instanceof String) {
-            return new StringValue((String) json);
         } else {
             return new StringValue(json.toString());
         }
@@ -95,12 +77,10 @@ public class JsonConverter {
         return jsonObject;
     }
 
+
     public static JSONObject structToJson(StructValue struct) {
-        JSONObject obj = new JSONObject();
-        for (String key : struct.getLabels()) {
-            obj.put(key, valueToJson(struct.getValue(key)));
-        }
-        return obj;
+        String jsonString = struct.toJson();
+        return new JSONObject(jsonString);
     }
 
     public static void mapJsonToVariables(JSONObject responseJson, String mappingsStr, Function<String, Slot> slotProvider) {
@@ -126,7 +106,10 @@ public class JsonConverter {
             try {
                 if (responseJson.has(jsonKey)) {
                     Object jsonValue = responseJson.get(jsonKey);
-                    Value dialogosValue = jsonToValue(jsonValue);
+                    String jsonString = (jsonValue instanceof JSONObject || jsonValue instanceof JSONArray) 
+                        ? jsonValue.toString() 
+                        : String.valueOf(jsonValue);
+                    Value dialogosValue = Value.fromJson(jsonString);
                     Slot targetSlot = slotProvider.apply(varName);
                     if (targetSlot != null) {
                         targetSlot.setValue(dialogosValue);
@@ -143,13 +126,6 @@ public class JsonConverter {
         }
     }
 
-    /**
-     * Maps entire JSON response to a single variable as Struct or String.
-     * @param responseJson The JSON response object
-     * @param targetVarName The target variable name
-     * @param asString If true, stores as String; if false, stores as StructValue
-     * @param slotProvider Function to get a slot by variable name
-     */
     public static void mapJsonToSingleVariable(JSONObject responseJson, String targetVarName, boolean asString, Function<String, Slot> slotProvider) {
         if (targetVarName == null || targetVarName.trim().isEmpty()) {
             System.out.println("Warning: No target variable specified");
@@ -170,7 +146,7 @@ public class JsonConverter {
                 System.out.println("Response stored as String in variable '" + targetVarName + "'");
             } else {
                 // Store as Struct
-                value = jsonToValue(responseJson);
+                value = Value.fromJson(responseJson.toString());
                 System.out.println("Response stored as Struct in variable '" + targetVarName + "'");
             }
             
